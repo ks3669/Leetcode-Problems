1147a994e9c156cd6a98a3b860424fc01e99dac5
ks3669 <157922493+ks3669@users.noreply.github.com>
2025-01-09 23:53:11 -0700

    Update GroupAnagrams.py
    Adding comments to explain my approach

--- GroupAnagrams.py
+++ GroupAnagrams.py
@@ -1,3 +1,15 @@
+'''
+Leetcode 49. Group Anagrams
+So basically my first thought was to compute and compare the ASCII values of each character
+and in that way if the sum in same then it will be a anagram.But later when I explored 
+different scenarios then I found out mistake. For example this method wont work for repeating consecutive letters like "aaa", or even "aab"
+So my next thought was to have some kind of a mapping for each character and if it is consistent then we can for sure tell that it is an anagram.
+So I implimented the same logic with dictionaries in python.
+
+Time Complexity - O(n)
+Space Complexity - O(n * k) where 'n' is the number of strings in the list and 'k' is the length of each string
+'''
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # Dictionary to map a character-count key to a list of anagrams
