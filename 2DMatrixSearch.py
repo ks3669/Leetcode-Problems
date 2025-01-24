cc97936544ccfb95ae789503bc989ad4bebc36d3
ks3669 <157922493+ks3669@users.noreply.github.com>
2025-01-23 18:25:20 -0700

    Update 2DMatrixSearch.py
    Added optimized solution and also comments about time complexity

--- 2DMatrixSearch.py
+++ 2DMatrixSearch.py
@@ -1,3 +1,7 @@
+'''
+The below solution will be of time complexity o m(log n) where M is the number of rows and N is the number of columns
+Because we have 1 for loop which iterates through teh rows and we have a binary search solution which will go through the columns hence logN
+'''
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         for i in range (len(matrix)):
@@ -16,4 +20,34 @@ class Solution:
                 return True
             else:
                 continue
+        return False
+
+
+
+'''
+The below code runs at o log(m*n) because the binary search is applied to both rows and columns
+'''
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        low = 0
+        high = len(matrix) - 1
+        while (low <= high) :
+            mid = low + (high - low) // 2
+            if (target >= matrix[mid][0]) and (target <= matrix[mid][-1]):
+                low1 = 0
+                high1 = len(matrix[mid])
+                while( low1 <= high1):
+                    mid1 = low1 + (high1 - low1) // 2
+                    if matrix[mid][mid1] == target:
+                        return True
+                    elif matrix[mid][mid1] > target:
+                        high1 = mid1 - 1
+                    else:
+                        low1 = mid1 + 1
+                return False
+            elif matrix[mid][0] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
         return False
